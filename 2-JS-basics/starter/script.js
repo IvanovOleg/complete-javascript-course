e4c43bfaefaf92a3468c86ddea9dd3f656b21789
IvanovOleg <olivanov87@gmail.com>
2019-10-06 21:01:57 +0300

    Variables and datatypes

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+console.log('Hello World!!!');
+
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(age);
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
